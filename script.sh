42e1b9dd8116ca5d52e10f7788501526f713e97f
Tweetie3132 <112626711+Tweetie3132@users.noreply.github.com>
2025-05-20 11:37:56 +0330

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,49 @@
+# Set VSCode as default for most programming languages(Obj-c and Swift are optional)
+duti -s com.microsoft.VSCode .py all       # Python
+duti -s com.microsoft.VSCode .js all       # JavaScript
+duti -s com.microsoft.VSCode .ts all       # TypeScript
+duti -s com.microsoft.VSCode .jsx all      # React JSX
+duti -s com.microsoft.VSCode .tsx all      # React TSX
+duti -s com.microsoft.VSCode .java all     # Java
+duti -s com.microsoft.VSCode .rb all       # Ruby
+duti -s com.microsoft.VSCode .php all      # PHP
+duti -s com.microsoft.VSCode .go all       # Go
+duti -s com.microsoft.VSCode .rs all       # Rust
+duti -s com.microsoft.VSCode .c all        # C
+duti -s com.microsoft.VSCode .cpp all      # C++
+duti -s com.microsoft.VSCode .cs all       # C#
+duti -s com.microsoft.VSCode .kt all       # Kotlin
+duti -s com.microsoft.VSCode .mjs all      # JavaScript Module
+duti -s com.microsoft.VSCode .json all     # JSON
+duti -s com.microsoft.VSCode .yaml all     # YAML
+duti -s com.microsoft.VSCode .yml all      # YAML (alt)
+duti -s com.microsoft.VSCode .css all      # CSS
+duti -s com.microsoft.VSCode .scss all     # Sass
+duti -s com.microsoft.VSCode .less all     # Less
+duti -s com.microsoft.VSCode .xml all      # XML
+duti -s com.microsoft.VSCode .sh all       # Shell script
+duti -s com.microsoft.VSCode .bash all     # Bash script
+duti -s com.microsoft.VSCode .zsh all      # Zsh script
+duti -s com.microsoft.VSCode .toml all     # TOML
+duti -s com.microsoft.VSCode .ini all      # INI
+duti -s com.microsoft.VSCode .env all      # Environment files
+duti -s com.microsoft.VSCode .md all       # Markdown
+duti -s com.microsoft.VSCode .txt all      # Plain Text
+duti -s com.microsoft.VSCode .vue all      # Vue.js
+duti -s com.microsoft.VSCode .svelte all   # Svelte
+duti -s com.microsoft.VSCode .dart all     # Dart (Flutter)
+duti -s com.microsoft.VSCode .lua all      # Lua
+duti -s com.microsoft.VSCode .r all        # R
+duti -s com.microsoft.VSCode .sql all      # SQL
+duti -s com.microsoft.VSCode .pl all       # Perl
+duti -s com.microsoft.VSCode .erl all      # Erlang
+duti -s com.microsoft.VSCode .ex all       # Elixir
+duti -s com.microsoft.VSCode .exs all      # Elixir script
+duti -s com.microsoft.VSCode .clj all      # Clojure
+duti -s com.microsoft.VSCode .cljs all     # ClojureScript
+duti -s com.microsoft.VSCode .scala all    # Scala
+duti -s com.microsoft.VSCode .h all         # Header files
+
+# Set VSCode as default for Swift and Objective-C files
+#duti -s com.microsoft.VSCode .swift all     # Swift
+#duti -s com.microsoft.VSCode .m all         # Objective-C implementation files
